004b6bc828cb2b9ddb14922f889746f565d4640d
Gabrielsousaa <gsousax2008@hotmail.com>
2022-06-29 17:41:25 -0300

    Alterando o header e fazendo o logout

--- dao/UserDAO.php
+++ dao/UserDAO.php
@@ -56,6 +56,19 @@ class UserDAO implements UserDAOInterface
   }
   public function verifyToken($protected = false)
   {
+    if (!empty($_SESSION["token"])) {
+      //
+      $token = $_SESSION["token"];
+      $user = $this->findByToken($token);
+
+      if ($user) {
+        return $user;
+      } else if ($protected) {
+        $this->message->setMessage("Logue para entrar nessa pagina", "error", "index.php");
+      }
+    } else if ($protected) {
+      $this->message->setMessage("Logue para entrar nessa pagina", "error", "index.php");
+    }
   }
   public function setTokenToSession($token, $redirect = true)
   {
@@ -94,6 +107,31 @@ class UserDAO implements UserDAOInterface
   }
   public function findByToken($token)
   {
+    if ($token != "") {
+
+      $stmt = $this->conn->prepare("SELECT * FROM users WHERE token = :token");
+      $stmt->bindParam(":token", $token);
+      $stmt->execute();
+
+      if ($stmt->rowCount() > 0) {
+
+        $data = $stmt->fetch();
+        $user = $this->buildUser($data);
+
+        return $user;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+
+  public function destroyToken()
+  {
+    $_SESSION["token"] = "";
+
+    $this->message->setMessage("Voce fez o logout com sucesso!", "success", "index.php");
   }
   public function changePassword(User $user)
   {
--- editprofile.php
+++ editprofile.php
@@ -1,5 +1,10 @@
 <?php
 require_once("templates/header.php");
+require_once("dao/UserDAO.php");
+
+$userDao = new UserDAO($conn, $BASE_URL);
+$userData = $userDAO->verifyToken(true);
+
 ?>
 <div id="main-container" class="container-fluid">
   <h1>Edição do usuario</h1>
